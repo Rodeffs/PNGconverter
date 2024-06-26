bc3af8ea8f7ecebfb0304fc720f54ad86a84b4d0
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-06-26 19:02:14 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <png.h>  // the libpng library used to read/write PNGs
+
+using std::cin, std::cout, std::endl;
+
+int main(int argc, char *argv[]) {
+	
+
+	return 0;
+}
